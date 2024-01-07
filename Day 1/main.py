066753e14e03cca3987a006394113edab2d9544c
nuthan <inbox.nuthan@gmail.com>
2024-01-08 02:33:15 +0530

    Day 1: practised on how to use print, variables, input functions

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,1 +1,15 @@
-print("Enter a todo")
+#Approch 1: print statement without any variables
+print("Print this string without variable in a print()")
+
+#Approach 2: print statement holds a variable
+user_prompt = "Display this string using a variable"
+print(user_prompt)
+
+#Approach 3: Text/string without print() acheive using input() 
+display_string_using_input=input("Display this string using input() function")
+print(display_string_using_input)
+
+#Approach 4: Text/string assign this to a variable and use that variable inside input()
+input_prompt = "Display this string/text using a variable but binding using input() function"
+input_variable_output = input(input_prompt)
+print(input_variable_output)
