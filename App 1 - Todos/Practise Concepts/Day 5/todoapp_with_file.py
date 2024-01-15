1289b7290b0c692da1cff7511d26202ba4fd9413
nuthan <inbox.nuthan@gmail.com>
2024-01-15 21:34:49 +0530

    1. Todoapp - We are storing this todo item in a file 2. Added a new file to store todoapp items 3. Added new error information in errors.txt file 4. Added file related questions in new quiz file under beginners section.

---
+++ App 1 - Todos/Practise Concepts/Day 5/todoapp_with_file.py
@@ -0,0 +1,31 @@
+# Storing data in file app
+
+# Create a new todos empty list array
+todos = []
+
+# Create a file object
+file = open('todos.txt','w')
+# Until you break the loop this will ask you to choose the action
+while True:
+    user_action = input("Todo App - Please select your action: add, edit, complete, show or exit: ")
+    user_action = user_action.strip()
+    match user_action:
+        case "add":
+            user_input = input("Add your todo item: ") + "\n"
+            todos.append(f"{len(todos) + 1}.{user_input}")
+            file = open('todos.txt','w')
+            file.writelines(todos)
+        case "edit":
+            user_input_index = int(input("Which item you want to modify? "))
+            updated_text = input("Please enter updated item: ")
+            todos[user_input_index - 1] = updated_text
+        case "show":
+            for index, item in enumerate(todos):
+                print(f"{index + 1}.{item.capitalize()}")
+        case "complete":
+            user_input_delete_index = int(input("Which item you want to set it as complete: "))
+            todos.pop(user_input_delete_index - 1)
+        case "exit":
+            break
+
+print("Will catchup soon!!!")
